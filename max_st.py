928fcdee768728f852c970af9fe1fa0029e22e78
Varun Jadia <jvarun@berkeley.edu>
2020-04-27 15:10:12 -0700

    updated command to run on all inputs

--- max_st.py
+++ max_st.py
@@ -1,5 +1,6 @@
 from parse import *
 import networkx as nx
+import solver_hl
 import os
 
 if __name__ == "__main__":
@@ -8,5 +9,5 @@ if __name__ == "__main__":
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
         G = read_input_file(f"{input_dir}/{input_path}")
-        T = nx.maximum_spanning_tree(G)
+        T = solver_hl.solve(G)
         write_output_file(T, f"{output_dir}/{graph_name}.out")
