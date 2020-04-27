57f4b953ac73bb8a1c4c980af227ca3739e978bd
Varun Jadia <jvarun@berkeley.edu>
2020-04-27 15:55:00 -0700

    pls

--- max_st.py
+++ max_st.py
@@ -4,7 +4,7 @@ import solver_hl
 import os
 
 if __name__ == "__main__":
-    output_dir = "submission"
+    output_dir = "output"
     input_dir = "inputs"
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
